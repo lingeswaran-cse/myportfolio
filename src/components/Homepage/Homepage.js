99ba0e61d387e2b984c2c79f8b27d5079cbc613b
Lingeswaran R <111352323+lingeswaran-cse@users.noreply.github.com>
2023-06-27 18:10:56 +0530

    Add files via upload

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import '../../pages/style.css';
+import { Container, Row, Col } from 'react-bootstrap'
+import Text from '../Homepage/Text'
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home() {
+  return (
+    <div className='homepagebackground'>
+      <Container>
+        <Row>
+          <Col md={7}>
+            <h2 className='headtext'>Hello <span className='wave'>👋 </span></h2>
+            <h2 className='nametext'>I'm Lingeswaran R</h2>
+            <span></span>
+            <Text />
+            <button onClick={() => {
+              window.open("https://github.com/lingeswaran-cse");
+            }}
+              className='socailmediabtn'><AiFillGithub className='icon' /></button>
+            <button onClick={() => {
+              window.open("https://www.linkedin.com/in/lingeswaran-r-039924248/");
+            }}
+              className='socailmediabtn'><FaLinkedinIn className='icon' /></button>
+            <button onClick={() => {
+              window.open("https://twitter.com/Lingeswaran_R5");
+            }}
+              className='socailmediabtn'><AiOutlineTwitter className='icon' /></button>
+            <button onClick={() => {
+              window.open("https://instagram.com/lingaa_19");
+            }}
+              className='socailmediabtn'><AiFillInstagram className='icon' /></button>
+          </Col>
+
+          <Col md={5}>
+            <div className="imagedeveloper">
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Projectlist/Projectlist.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../pages/style.css';
+import ProjectCard from "./ProjectCard";
+import { Container, Row, Col } from 'react-bootstrap'
+import useradminpanel from '../../Assets/Admin-Panel-Dashboard.png'
+import olximage from '../../Assets/olx-clone image.png'
+import netfliximage from '../../Assets/React-App-netflix-clone.png'
+import gridlinesbuilders from '../../Assets/Gridlinesbuilders.png'
+import charlespizza from '../../Assets/Charlestown-Pizza.png'
+import todolist from '../../Assets/todolist.png'
+
+function Projectlist() {
+  return (
+    <div className="projectbackground">
+  
+    </div>
+  )
+}
+export default Projectlist
